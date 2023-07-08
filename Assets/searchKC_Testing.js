7fdd708875ba5cd30bc71f45a7686c7dfcbe18e2
Katherine Clark <60265279+KClarkDev@users.noreply.github.com>
2023-07-08 17:22:19 -0400

    Adding test js file

---
+++ Assets/searchKC_Testing.js
@@ -0,0 +1,35 @@
+var apiUrl = "https://nominatim.openstreetmap.org/search?amenity=";
+var ul = document.createElement("ul");
+var data = {
+  name: "Store",
+};
+
+var fetchData = {
+  method: "GET",
+  // body: JSON.stringify(data),
+  headers: new Headers({
+    "Content-Type": "application/json; charset=UTF-8",
+  }),
+};
+
+fetch("https://nominatim.openstreetmap.org/search?q=garden/")
+  .then((response) => {
+    //handle the response
+    return response.json();
+  })
+  .then((data) => {
+    var stores = data;
+    console.log(stores);
+    stores.map(function (stores) {
+      var li = document.createElement("li");
+      var name = document.createElement("h3");
+
+      name.innerHTML = `${stores.name}`;
+
+      li.appendChild(name);
+      list.appendChild(li);
+    });
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
